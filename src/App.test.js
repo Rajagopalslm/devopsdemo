985885b6ce0ebe4bf6a867838611f35487eebb36
Rajagopal <msrgpl@gmail.com>
2025-08-30 23:34:15 +0530

    Fix test setup and imports

--- src/App.test.js
+++ src/App.test.js
@@ -1,4 +1,5 @@
 import { render, screen, fireEvent } from '@testing-library/react';
+import '@testing-library/jest-dom';
 import App from './App';
 
 test('renders DevOps Demo Application title', () => {
